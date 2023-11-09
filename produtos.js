e9fa7a6c8d1881b469882d081b70e0698668cc07
Dnzinnn7 <140813753+Dnzinnn7@users.noreply.github.com>
2023-11-09 11:58:56 -0300

    caarrinho de compras

---
+++ produtos.js
@@ -0,0 +1,8 @@
+function Remove(){
+document.querySelector(".produto-carrinho").textContent = " ";
+}
+
+function Adicione(){
+    console.log("teste");
+    document.querySelector(".novoItem").textContent = document.querySelector(".produto").textContent;
+}
